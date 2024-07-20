13bd7c8a9f0a4d232aa568ec0c6c17eca1b53a4f
muzzu <www.mdmozammil@gmail.com>
2024-07-21 00:18:17 +0530

    error handler function added

--- api/controllers/auth.control.js
+++ api/controllers/auth.control.js
@@ -1,17 +1,19 @@
 import expres from 'express';
-import User from '../models/user.model.js'
+import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import   {errorHandler}  from '../utils/error.js';
 
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { name, email, password } = req.body;
     const hashedPassword= bcrypt.hashSync(password,10);
     const user = new User({ name, email, password:hashedPassword });
     try {
         await user.save();
-        res.status(201).json(user);
-    } catch (error) {
-        res.status(409).json({ message: error.message });
+        res.status(201).json("user created successfully");
+    } 
+    catch (error) {
+        next(errorHandler(502,"user creating server error"));
     }
 }
 
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -3,6 +3,6 @@ import test from "../controllers/user.control.js";
 
 const router = expres.Router();
 
-router.get('/', test);
+router.get('/test', test);
 
 export default router;
