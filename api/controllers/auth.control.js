05bca45b3372286650b0d4907bdb96ba1b732cdb
muzzu <www.mdmozammil@gmail.com>
2024-07-21 02:48:37 +0530

    create signin api route

--- api/controllers/auth.control.js
+++ api/controllers/auth.control.js
@@ -2,6 +2,9 @@ import expres from 'express';
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import   {errorHandler}  from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 export const signup = async (req, res, next) => {
@@ -17,3 +20,26 @@ export const signup = async (req, res, next) => {
     }
 }
 
+
+
+export const signin = async (req,res,next)=>{
+    const {email,password} = req.body;
+    try{
+        const uservalidate = await User.findOne({email});
+        if(!uservalidate) return next(errorHandler(502,"Invalid UserId or Password"));
+        const passwordValidate = bcrypt.compareSync(password,uservalidate.password);
+        
+        if(!passwordValidate) return next(errorHandler(502,"Invalid UserId or Password"));
+        const {password:pass, ...restdata} = uservalidate._doc;
+        const token = jwt.sign({id:uservalidate._id},process.env.JWT_SECRET)
+        res
+        .cookie('token',token,{httpOnly:true, expire:new Date()+9999})
+        .status(200)
+        .json(restdata);
+        
+    }
+    catch(error){
+        next(errorHandler(502,"Bad Credentials"));
+
+    }
+}
