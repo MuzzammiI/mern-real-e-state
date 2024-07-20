8b35dddf557add67504709bc5e44c4d0d7cc878d
muzzu <www.mdmozammil@gmail.com>
2024-07-20 13:36:29 +0530

    create routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import {BrowserRouter as Router, Route, BrowserRouter, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
  
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
